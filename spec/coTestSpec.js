ea938acc2693fe3da1f15516db195a798c3e442d
Alejo Ortiz <davidalejandro64@gmail.com>
2020-11-06 19:29:42 -0500

    create tests to Special Full Coverage produt, all ok

--- spec/coTestSpec.js
+++ spec/coTestSpec.js
@@ -80,3 +80,63 @@ describe("Co Test for Mega Coverege", function() {
     expect(products[0].price).equal(80);
   });
 });
+
+describe("Co Test for Special Full Coverage", function() {
+
+  it("should return  14, 21 for sellIn and price on day 1", function() {
+    const coTest = new CarInsurance([ new Product("Special Full Coverage", 15, 20) ]);
+    const products = coTest.updatePrice();
+    expect(products[0].name).equal("Special Full Coverage");
+    expect(products[0].sellIn).equal(14);
+    expect(products[0].price).equal(21);
+  });
+
+  it("should return  12, 23 for sellIn and price on day 3", function() {
+    const coTest = new CarInsurance([ new Product("Special Full Coverage", 13, 22) ]);
+    const products = coTest.updatePrice();
+    expect(products[0].name).equal("Special Full Coverage");
+    expect(products[0].sellIn).equal(12);
+    expect(products[0].price).equal(23);
+  });
+
+  it("should return  10, 25 for sellIn and price on day 5", function() {
+    const coTest = new CarInsurance([ new Product("Special Full Coverage", 11, 24) ]);
+    const products = coTest.updatePrice();
+    expect(products[0].name).equal("Special Full Coverage");
+    expect(products[0].sellIn).equal(10);
+    expect(products[0].price).equal(25);
+  });
+
+  it("should return  7, 31 for sellIn and price on day 8", function() {
+    const coTest = new CarInsurance([ new Product("Special Full Coverage", 8, 29) ]);
+    const products = coTest.updatePrice();
+    expect(products[0].name).equal("Special Full Coverage");
+    expect(products[0].sellIn).equal(7);
+    expect(products[0].price).equal(31);
+  });
+
+  it("should return  0, 50 for sellIn and price on day 15", function() {
+    const coTest = new CarInsurance([ new Product("Special Full Coverage", 1, 47) ]);
+    const products = coTest.updatePrice();
+    expect(products[0].name).equal("Special Full Coverage");
+    expect(products[0].sellIn).equal(0);
+    expect(products[0].price).equal(50);
+  });
+
+  it("should return  -7, 0 for sellIn and price on day 22", function() {
+    const coTest = new CarInsurance([ new Product("Special Full Coverage", -6, 0) ]);
+    const products = coTest.updatePrice();
+    expect(products[0].name).equal("Special Full Coverage");
+    expect(products[0].sellIn).equal(-7);
+    expect(products[0].price).equal(0);
+  });
+
+  it("should return  -15, 0 for sellIn and price on day 30", function() {
+    const coTest = new CarInsurance([ new Product("Special Full Coverage", -14, 0) ]);
+    const products = coTest.updatePrice();
+    expect(products[0].name).equal("Special Full Coverage");
+    expect(products[0].sellIn).equal(-15);
+    expect(products[0].price).equal(0);
+  });
+
+});
